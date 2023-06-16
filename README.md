04f7b19481ff95083fd8a23247000bdcea707c29
Prince <you@example.com>
2023-06-16 13:39:30 +0100

    The Age Calculator

---
+++ README.md
@@ -0,0 +1,75 @@
+# Frontend Mentor - Age calculator app solution
+
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+The age calculator is designed to calculate your age from your birthday to current day.
+I calculates for age in years, months and days.
+
+User is to input his/her birthdates in the input sections and click the button to display calculated age, after clicking to display age click button again to reset all values.
+
+You can share with your friends and Enjoy 🎉
+
+### The challenge
+
+Users should be able to:
+
+- View an age in years, months, and days after submitting a valid date through the form
+- Receive validation errors if:
+  - Any field is empty when the form is submitted
+  - The day number is not between 1-31
+  - The month number is not between 1-12
+  - The year is in the future
+  - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- **Bonus**: See the age numbers animate to their final number when the form is submitted
+
+### Screenshot
+
+![](./design/desktop-completed.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://)
+- Live Site URL: [Add live site URL here](https://princecreatez-agecalculator.vercel.app)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Java Script
+
+### What I learned
+
+Basiclly what i enjoyed in this project was calling functions, although it was not new to me but i really enjoyed it and learned some new stuff from calling functions like writing more than one funtion in function and calling them a once in an event handler function.
+
+### Continued development
+
+I would love to continue with projects that has more complex logic than this one to improve my skills and will also learn tailwind css frame work to help in my front-end skills.
+
+## Author
+
+- Website - [Prince Createz](https://princecreatez.netlify.app)
+- Frontend Mentor - [@Princecodes205](https://www.frontendmentor.io/Princecodes205/@Princecodes205)
+- Twitter - [@prince_champs](https://www.twitter.com/@prince_champs)
+
+## Acknowledgments
+
+A big thanks to my Team "Akpesiri, Laye, Okiemute, Dave" they are my motivation and they where a greate source of motivation to me and also contribute in one way or the order to my coding journey ❤.
---
+++ js/script.js
@@ -0,0 +1,184 @@
+"use strict";
+const checkBtn = document.querySelector(".svg");
+const calcBtn = document.querySelector(".calcb");
+const resetBtn = document.querySelector(".reset");
+const inputDay = document.querySelector(".day_input");
+const inputMonth = document.querySelector(".month_input");
+const inputYear = document.querySelector(".year_input");
+const textError = document.querySelectorAll(".texterror");
+const error = document.querySelectorAll(".errors");
+const resultsValues = document.querySelectorAll(".val");
+const inputs = [inputDay, inputMonth, inputYear];
+
+const currentDate = new Date();
+const day = currentDate.getDate();
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1;
+const premonth = currentDate.getMonth();
+const numberOfDaysInCurrentMonth = new Date(year, month, 0).getDate();
+//Get number of days in previous month
+const numberOfDaysInPreviousMonth = new Date(year, premonth, 0).getDate();
+
+// validate errors
+const checkErrors = function () {
+  const checkDay = function () {
+    if (inputDay.value > 31 || inputDay.value < 1) {
+      error[0].textContent = "Must be a valid date";
+      inputs[0].classList.add("inputerr");
+      textError[0].style.color = " hsl(0, 100%, 67%)";
+    }
+  };
+  const checkMonth = function () {
+    if (inputMonth.value > 12) {
+      error[1].textContent = "Must be a valid Month";
+      inputs[1].classList.add("inputerr");
+      textError[1].style.color = " hsl(0, 100%, 67%)";
+    }
+  };
+  const checkYear = function () {
+    if (inputYear.value > year) {
+      error[2].textContent = "Must be a valid Year";
+      inputs[2].classList.add("inputerr");
+      textError[2].style.color = " hsl(0, 100%, 67%)";
+    } else if (inputYear.value < 1899) {
+      error[2].textContent = "Must be a valid Year";
+      inputs[2].classList.add("inputerr");
+      textError[2].style.color = " hsl(0, 100%, 67%)";
+    }
+  };
+  checkDay();
+  checkMonth();
+  checkYear();
+};
+
+// calculation algorithm
+const calculations = function () {
+  const calcYear = function () {
+    const result = year - Number(inputYear.value);
+    if (month < inputMonth.value) {
+      resultsValues[0].textContent = result - 1;
+    } else {
+      resultsValues[0].textContent = result;
+    }
+  };
+  const calcMonth = function () {
+    if (day < inputDay.value) {
+      resultsValues[1].textContent = month + 12 - Number(inputMonth.value) - 1;
+    } else if (month < inputMonth.value) {
+      resultsValues[1].textContent = month + 12 - Number(inputMonth.value);
+    } else {
+      resultsValues[1].textContent = month - Number(inputMonth.value);
+    }
+  };
+  const calcDay = function () {
+    if (day >= inputDay.value) {
+      resultsValues[2].textContent = day - Number(inputDay.value);
+    } else if (day < inputDay.value) {
+      resultsValues[2].textContent =
+        Number(numberOfDaysInPreviousMonth) + day - Number(inputDay.value);
+    }
+  };
+
+  calcYear();
+  calcMonth();
+  calcDay();
+};
+
+/// implementing the algorithm
+const wholeFunc = function () {
+  const dontCalculate = function () {
+    if (
+      inputs[0].value === "" &&
+      inputs[1].value === "" &&
+      inputs[2].value === ""
+    ) {
+      return;
+    } else if (
+      inputs[0].value !== "" &&
+      inputs[1].value !== "" &&
+      inputs[2].value !== ""
+    ) {
+      resetBtn.addEventListener("click", function () {
+        resultsValues[0].textContent = "--";
+        resultsValues[1].textContent = "--";
+        resultsValues[2].textContent = "--";
+        inputs.forEach((el) => {
+          el.value = "";
+        });
+        checkBtn.classList.remove("hidden");
+      });
+      calculations();
+    }
+  };
+
+  inputs.forEach((inp, i) => {
+    error.forEach((el, e) => {
+      textError.forEach((te, t) => {
+        if (inp.value === "" && e === i && t === e) {
+          inp.classList.add("inputerr");
+          el.textContent = "Field must not be empty ";
+          te.style.color = " hsl(0, 100%, 67%)";
+          te.style.margin = "0";
+        } else if (inp.value !== "" && e === i) {
+          inp.classList.remove("inputerr");
+          el.textContent = "";
+          te.classList.remove("texterr");
+          te.style.color = " hsl(0, 0%, 86%)";
+          te.style.margin = "0";
+          checkErrors();
+        }
+      });
+      dontCalculate();
+    });
+  });
+};
+
+resultsValues.forEach((el) => {
+  el.classList.remove("animate");
+});
+
+inputs.forEach((el) => {
+  el.addEventListener("click", function () {
+    calcBtn.classList.remove("hidden");
+  });
+});
+
+resetBtn.addEventListener("click", function () {
+  const checkerrs = checkErrors();
+  if (checkerrs) {
+    return;
+  }
+  resetBtn.classList.add("hidden");
+  calcBtn.classList.add("hidden");
+  checkBtn.classList.remove("hidden");
+});
+calcBtn.addEventListener("click", function () {
+  wholeFunc();
+  const checkerr = checkErrors();
+
+  if (!checkerr) {
+    resetBtn.classList.add("hidden");
+  }
+  resetBtn.classList.remove("hidden");
+
+  resultsValues[0].classList.add("animate");
+  setTimeout(() => {
+    resultsValues[0].classList.remove("animate");
+  }, 2000);
+
+  resultsValues.forEach((el) => {
+    el.classList.add("animate");
+
+    setTimeout(() => {
+      el.classList.remove("animate");
+    }, 1000);
+  });
+  // resultsValues.forEach((el) => {
+  //   el.classList.add("animate");
+  // });
+});
+
+checkBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  wholeFunc();
+});
